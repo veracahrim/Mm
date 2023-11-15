6995070d17cbbb6841b2455b858f5b183b571b12
veracahrim <patrickbrunner96@hotmail.ch>
2023-11-15 10:29:50 +0000

    add stuff

--- mm/src/App.js
+++ mm/src/App.js
@@ -1,22 +1,16 @@
-import logo from './logo.svg';
+import React, {useEffect, useState} from 'react';
+import data from './sot.json';
 import './App.css';
 
 function App() {
+
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        {data?.length && data.map(card => {
+          return <p>{card?.name}</p>
+        })}
       </header>
     </div>
   );
